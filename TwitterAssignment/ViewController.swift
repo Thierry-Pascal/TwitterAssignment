57b46ab44c27b020c3f682d05a582855b8b4ca7f
Thierry-Pascal <thierryfleurant18@gmail.com>
2022-09-30 22:33:30 -0400

    Initial Commit

---
+++ TwitterAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterAssignment
+//
+//  Created by Thierry-Pascal Fleurant on 9/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
